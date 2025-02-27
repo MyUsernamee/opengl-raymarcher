02d57cc2550d74efbb2c0ea7f7c729110645777d
MyUsernamee <delanoleslieprof@gmail.com>
2025-02-27 14:01:31 -0800

    Working gl clear, we forgot to actually call clear lmao

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include "../include/glad/glad.h"
+#include <GLFW/glfw3.h>
 
 int main() {
-	printf("Hello, World!\n");
+
+  glfwInit();
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello World!", nullptr, nullptr);
+
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+    return 1; 
+  glfwSwapInterval(1);
+
+  while (!glfwWindowShouldClose(window)){
+
+    glViewport(0, 0, 1280, 1280);
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
 	return 0;
 }
